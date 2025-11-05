fc82963aa6e0a0a86794d500c144f5fc38f261a8
Stepan <bolshakov.ca@gmail.com>
2025-11-05 13:26:47 +0700

    Added logging config

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
+import logging
 import uvicorn
 
 from fastapi import FastAPI
+from app.core import settings
+logging.basicConfig(
+    level=settings.logging.log_level_value,
+    format=settings.logging.log_format,
+    datefmt=settings.logging.date_format,
+)
 
 
 app = FastAPI(
